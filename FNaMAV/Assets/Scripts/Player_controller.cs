357e0be58ee19b078e2eafbd986c6bd875ddb4c3
MaM0Rik <74770687+MaM0Rik@users.noreply.github.com>
2022-02-11 00:48:20 +0700

    FNaMAV (0.0.0.4)

---
+++ FNaMAV/Assets/Scripts/Player_controller.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_controller : MonoBehaviour
+{
+    public Transform PlayerBody;
+
+    public GameObject B_left;
+    public GameObject B_right;
+    public GameObject B_camera_up;
+
+    public GameObject Tablet;
+    public GameObject Flashlight;
+    
+    public float RotationSpeed = 8f;
+
+    public AudioSource flashlightAudeo;
+    public int R = 1;
+    public int C = 0;
+
+    void Update() {
+    ///Debug.Log(R);
+
+        if(R == 2){
+            PlayerBody.transform.rotation = Quaternion.Lerp(PlayerBody.transform.rotation, Quaternion.AngleAxis(-90, Vector3.up), RotationSpeed * Time.deltaTime);
+            B_camera_up.SetActive(false);
+            B_left.SetActive(false);
+            B_right.SetActive(true);
+
+            Flashlight.SetActive(false);
+        }
+
+        if(R == 0){
+            PlayerBody.transform.rotation = Quaternion.Lerp(PlayerBody.transform.rotation, Quaternion.AngleAxis(90, Vector3.up), RotationSpeed *Time.deltaTime);
+            B_left.SetActive(true);
+            B_right.SetActive(false);
+            B_camera_up.SetActive(false);
+
+            Flashlight.SetActive(true);
+            ///flashlightAudeo.Play();
+        }
+
+        if(R == 1){
+            PlayerBody.transform.rotation = Quaternion.Lerp(PlayerBody.transform.rotation, Quaternion.AngleAxis(0, Vector3.up), RotationSpeed * Time.deltaTime);
+            B_left.SetActive(true);
+            B_right.SetActive(true);
+            B_camera_up.SetActive(true);
+
+            Flashlight.SetActive(false);
+            if(C == 0){
+                Tablet.SetActive(false);
+                B_left.SetActive(true);
+                B_right.SetActive(true);
+            }
+            if(C == 1){
+                Tablet.SetActive(true);
+                B_left.SetActive(false);
+                B_right.SetActive(false);
+            }
+            if(C == 2){
+                C = 0;
+            }
+        }
+    }
+
+    public void Button_left(){
+        R += 1;
+    }
+
+    public void Button_right(){
+        R -= 1;
+    }
+    public void Button_camera_up(){
+        C += 1;
+    }
+}
