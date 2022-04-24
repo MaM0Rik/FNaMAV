c8b01b6a6b5bcda6c6d3eb579afbb874172ca588
MaM0Rik <74770687+MaM0Rik@users.noreply.github.com>
2022-04-24 13:59:57 +0700

    FNaMAV (0.0.1.8)

--- FNaMAV/Assets/Scripts/Luke_controller.cs
+++ FNaMAV/Assets/Scripts/Luke_controller.cs
@@ -6,6 +6,9 @@ public class Luke_controller : MonoBehaviour
 {
     public int Luke_status = 0;
     public GameObject Luke_obj;
+    public float speed = 0.1f;
+    public Transform a_point;
+    public Transform b_point;
 
     void Update()
     {
@@ -20,13 +23,13 @@ public class Luke_controller : MonoBehaviour
         }
 
         if(Luke_status == 1){
-
+            Luke_obj.transform.position = Vector3.Lerp(a_point.position, b_point.position, speed);
         }
         if(Luke_status == 2){
             Luke_status = 0;
         }
         if(Luke_status == 0){
-
+            Luke_obj.transform.position = Vector3.Lerp(b_point.position, a_point.position, speed);
         }
         
     }
